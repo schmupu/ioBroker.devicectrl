ae3e633020e015fd489658ce313eebb51c0993a1
Thorsten Stueben <thorsten.stueben@macbook.local>
2018-11-17 23:00:19 +0100

    First Version

--- ruleset.js
+++ ruleset.js
@@ -2,10 +2,11 @@
 
 var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
 var dp = require(__dirname + '/lib/datapoints');
-var rulesset  = require(__dirname + '/lib/rules');
+var rulesset = require(__dirname + '/lib/rules');
 var net = require('net');
 var adapter = new utils.Adapter('ruleset');
-
+var vm = require('vm');
+var rules = new rulesset(adapter);
 
 // *****************************************************************************************************
 // is called when adapter shuts down - callback has to be called under any circumstances!
@@ -28,7 +29,30 @@ adapter.on('unload', (callback) => {
 // Listen for sendTo messages
 // *****************************************************************************************************
 adapter.on('message', (msg) => {
- 
+
+  let command = msg.command;
+  let parameter = msg.message;
+  let callback = msg.callback;
+  let id = msg._id;
+
+  switch (command) {
+    case 'add':
+      rules.addRule(parameter);
+      break;
+    case 'delete':
+      rules.deleteRule(parameter);
+      break;
+    case 'modify':
+      rules.modifyRule(parameter);
+      break;
+    default:
+      break;
+  }
+
+  rules.executeRules((values) => {
+    // callback && callback(values);
+  });
+
 });
 
 
@@ -41,13 +65,11 @@ adapter.on('ready', () => {
 });
 
 
-
 // *****************************************************************************************************
 // Main function
 // *****************************************************************************************************
 function main() {
 
   adapter.log.info("Starting Adapter");
-  let rules = new rulesset();
 
 }
