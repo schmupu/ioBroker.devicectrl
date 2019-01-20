4b5d528cc4a83e97562d48d0dde4c0294d45014c
Thorsten Stueben <thorsten.stueben@macbook.local>
2019-01-20 20:51:01 +0100

    Update

--- devicectrl.js
+++ devicectrl.js
@@ -7,9 +7,12 @@
 
 const utils = require('@iobroker/adapter-core');
 const rulesset = require(__dirname + '/lib/rules');
-const request = require("request");
-const sched = require("node-schedule");
+const request = require('request');
+const sched = require('node-schedule');
+const semver = require('semver');
 const adapterName = require('./package.json').name.split('.').pop();
+const adapterNodeVer = require('./package.json').engines.node;
+
 let rules = null;
 let adapter;
 
@@ -31,7 +34,6 @@ function startAdapter(options) {
     }
   });
 
-
   // *****************************************************************************************************
   // Listen for sendTo messages
   // *****************************************************************************************************
@@ -41,17 +43,17 @@ function startAdapter(options) {
         let command = msg.command;
         let parameter = msg.message;
         let r = undefined;
-      
+
         switch (command) {
           case 'add':
             if (parameter) {
-              adapter.log.info("Add Rule : " + parameter.rulename);
+              adapter.log.info('Add Rule : ' + parameter.rulename);
               rules.addRule(parameter);
             }
             break;
           case 'delete':
             if (parameter) {
-              adapter.log.info("Delete Rule : " + parameter);
+              adapter.log.info('Delete Rule : ' + parameter);
               rules.deleteRule(parameter);
             }
             break;
@@ -73,13 +75,12 @@ function startAdapter(options) {
           default:
         }
         await executeRulesAsync(rules);
-        adapter.sendTo(msg.from, msg.command, "Execute command " + command, msg.callback);
+        adapter.sendTo(msg.from, msg.command, 'Execute command ' + command, msg.callback);
       } catch (error) {
         adapter.log.error(error);
       }
     })();
   });
-
 
   // *****************************************************************************************************
   // is called when databases are connected and adapter received configuration.
@@ -91,7 +92,6 @@ function startAdapter(options) {
   return adapter;
 }
 
-
 // *****************************************************************************************************
 // Read holidays Async
 // *****************************************************************************************************
@@ -99,7 +99,7 @@ async function getFeiertagAsync(state, year) {
   return new Promise((resolve, reject) => {
     if (state) {
       if (!year) year = (new Date()).getFullYear();
-      let url = "https://ipty.de/feiertag/api.php?do=getFeiertage&loc=" + state + "&jahr=" + year + "&outformat=Y-m-d";
+      let url = 'https://ipty.de/feiertag/api.php?do=getFeiertage&loc=' + state + '&jahr=' + year + '&outformat=Y-m-d';
       request({ url: url }, function (error, response, body) {
         if (body) {
           let result = JSON.parse(body);
@@ -128,19 +128,18 @@ async function getCoordnatesAsync() {
   }
 }
 
-
 // *****************************************************************************************************
 // Save Holidays
 // *****************************************************************************************************
 async function saveHolidaysAsync(holiday) {
   if (holiday) {
     try {
-      let id = "config.holiday";
+      let id = 'config.holiday';
       holiday = JSON.stringify(holiday);
       await adapter.setStateAsync(id, holiday, true);
-      adapter.log.info("Saving Holidays");
+      adapter.log.info('Saving Holidays');
     } catch (error) {
-      adapter.log.info("Error, saving Holidays");
+      adapter.log.info('Error, saving Holidays');
     }
   }
 }
@@ -150,6 +149,7 @@ async function saveHolidaysAsync(holiday) {
 // *****************************************************************************************************
 async function loadHolidayAsync() {
   try {
+    let id = 'config.holiday';
     let state = await adapter.getStateAsync(id);
     if (state && state.val) {
       state = JSON.parse(state.val);
@@ -168,13 +168,13 @@ async function loadHolidayAsync() {
 async function saveRulesSetAdpaterAsync(ruleset) {
   if (ruleset) {
     try {
-      let id = "config.ruleset";
+      let id = 'config.ruleset';
       ruleset = JSON.stringify(ruleset);
-      adapter.log.info("Saving Ruleset (Adapter)");
+      adapter.log.info('Saving Ruleset (Adapter)');
       await adapter.setStateAsync(id, ruleset, true);
-      adapter.log.info("Saving Ruleset successfull");
+      adapter.log.info('Saving Ruleset successfull');
     } catch (error) {
-      adapter.log.info("Nothing to save");
+      adapter.log.info('Nothing to save');
     }
   }
 }
@@ -184,8 +184,8 @@ async function saveRulesSetAdpaterAsync(ruleset) {
 // *****************************************************************************************************
 async function loadRulesSetAdapterAsync() {
   try {
-    let id = "config.ruleset";
-    adapter.log.info("Loading Ruleset (Adapter)");
+    let id = 'config.ruleset';
+    adapter.log.info('Loading Ruleset (Adapter)');
     let state = await adapter.getStateAsync(id);
     if (state && state.val) {
       state = JSON.parse(state.val);
@@ -204,14 +204,14 @@ async function loadRulesSetAdapterAsync() {
 async function saveRulesSetAsync(ruleset) {
   if (ruleset) {
     try {
-      let id = "system.adapter." + adapter.namespace;
-      adapter.log.info("Saving Ruleset");
+      let id = 'system.adapter.' + adapter.namespace;
+      adapter.log.info('Saving Ruleset');
       let obj = await adapter.getForeignObjectAsync(id);
       obj.native.ruleset = ruleset;
       await adapter.setForeignObject(id, obj);
-      adapter.log.info("Saving Ruleset successfull");
+      adapter.log.info('Saving Ruleset successfull');
     } catch (error) {
-      adapter.log.eror("Erro saving Ruleset");
+      adapter.log.eror('Erro saving Ruleset');
     }
   }
 }
@@ -221,10 +221,10 @@ async function saveRulesSetAsync(ruleset) {
 // *****************************************************************************************************
 async function loadRulesSetAsync() {
   try {
-    let id = "system.adapter." + adapter.namespace;
-    adapter.log.info("Loading Ruleset");
+    let id = 'system.adapter.' + adapter.namespace;
+    adapter.log.info('Loading Ruleset');
     let obj = await adapter.getForeignObjectAsync(id);
-    adapter.log.info("Loading Ruleset successfull");
+    adapter.log.info('Loading Ruleset successfull');
     return obj.native.ruleset || [];
   } catch (error) {
     return [];
@@ -240,7 +240,7 @@ function showRules(r) {
   for (let i in r) {
     if (r[i].rulename) {
       count++;
-      adapter.log.info(count + ".) Load Rule " + r[i].rulename + ", Aktiv: " + r[i].active);
+      adapter.log.info(count + '.) Load Rule ' + r[i].rulename + ', Aktiv: ' + r[i].active);
     }
   }
 }
@@ -257,9 +257,9 @@ function executeRulesAsync(rules) {
       if (values) {
         adapter.log.debug(JSON.stringify(values));
         if (simulation) {
-          // adapter.log.info("Simulation " + values.rulename + ", alte Regel " + values.oldRegel + ", neue Regel " + values.regel + ", von altem Wert " + values.oldValue + " auf neuen Wert " + values.value);                         
+          // adapter.log.info('Simulation ' + values.rulename + ', alte Regel ' + values.oldRegel + ', neue Regel ' + values.regel + ', von altem Wert ' + values.oldValue + ' auf neuen Wert ' + values.value);                         
         } else {
-          // adapter.log.info(values.rulename + ", alte Regel " + values.oldRegel + ", neue Regel " + values.regel + ", von altem Wert " + values.oldValue + " auf neuen Wert " + values.value);                                
+          // adapter.log.info(values.rulename + ', alte Regel ' + values.oldRegel + ', neue Regel ' + values.regel + ', von altem Wert ' + values.oldValue + ' auf neuen Wert ' + values.value);                                
         }
       }
 
@@ -269,33 +269,86 @@ function executeRulesAsync(rules) {
   })();
 }
 
+// *****************************************************************************************************
+// Intersection of 2 Arrays
+// *****************************************************************************************************
+function intersection(array1, array2) {
+  return array1.filter((value) => -1 !== array2.indexOf(value));
+}
+
+function intersectionArrays(arraylist) {
+  let array1 = arraylist.shift();
+  let array2;
+  while ((array2 = arraylist.shift())) {
+    array1 = intersection(array1, array2);
+  }
+  return array1;
+}
+
+// *****************************************************************************************************
+// Get List of all state id for enum
+// *****************************************************************************************************
+async function getStateIdfromEnums(enums) {
+  let ret = [];
+  let objs = await adapter.getForeignObjectsAsync('*', 'state');
+  enums = (typeof enums === 'string') ? [enums] : enums;
+  for (let o in objs) {
+    let e = objs[o].enums;
+    if (e && Object.keys(e).length !== 0) {
+      for (let i in enums) {
+        if (e.hasOwnProperty(enums[i])) {
+          ret.push(o);
+          break;
+        }
+      }
+    }
+  }
+  return ret;
+}
+
+async function test() {
+
+  let ems = await adapter.getEnumsAsync('functions.light');
+  adapter.log.info(JSON.stringify(ems));
+  let em = await adapter.getEnumAsync('functions.light');
+  adapter.log.info(JSON.stringify(em));
+
+  let objs1 = await getStateIdfromEnums('enum.functions.light');
+  let objs2 = await getStateIdfromEnums('enum.rooms.living_room');
+  let objs3 = intersectionArrays([objs2, objs1, objs2, objs1]);
+  adapter.log.info(JSON.stringify(objs3));
+
+}
 
 // *****************************************************************************************************
 // Main
 // *****************************************************************************************************
 function mainAsync() {
 
-  (async () => {
+  adapter.log.info('Starting Adapter ' + adapter.namespace + ' in version ' + adapter.version);
+  if (!semver.satisfies(process.version, adapterNodeVer)) {
+    adapter.log.error(`Required node version ${adapterNodeVer} not satisfied with current version ${process.version}.`);
+    return;
+  }
 
+  (async () => {
+    // await test();
     rules = new rulesset.RulesControlerAsync(adapter);
     let cal = adapter.config.holiday || 'HH';
-    let simulation = adapter.config.simulation || false;
-
-    adapter.log.info("Starting Adapter " + adapter.namespace + " in version " + adapter.version);
 
     //Get every Jear new Hollidays
     sched.scheduleJob('1 0 * * *', () => {
       (async () => {
         let holidays = await getFeiertagAsync(cal);
         if (holidays) {
-          adapter.log.info("Got new holidays");
+          adapter.log.info('Got new holidays');
           rules.setHolidays(holidays);
           await saveHolidaysAsync(holidays);
         }
         let coord = await getCoordnatesAsync();
         if (coord) {
           rules.setCoordinates(coord.latitude, coord.longitude);
-          adapter.log.info("Got coordinates!");
+          adapter.log.info('Got coordinates!');
         }
       })();
     });
@@ -303,18 +356,18 @@ function mainAsync() {
     // on every Start get Holidays
     let holidays = await getFeiertagAsync(cal);
     if (holidays) {
-      adapter.log.info("Got new holidays");
+      adapter.log.info('Got new holidays');
       rules.setHolidays(holidays);
       await saveHolidaysAsync(holidays);
     }
 
-    let coord = await getCoordnatesAsync()
+    let coord = await getCoordnatesAsync();
     if (coord) {
       rules.setCoordinates(coord.latitude, coord.longitude);
-      adapter.log.info("Got coordinates!");
+      adapter.log.info('Got coordinates!');
     }
 
-    let r = await loadRulesSetAsync()
+    let r = await loadRulesSetAsync();
     rules.addRules(r);
     // showRules();
     await executeRulesAsync(rules);
@@ -328,9 +381,8 @@ function mainAsync() {
 
 }
 
-
 // If started as allInOne mode => return function to create instance
-if (typeof module !== "undefined" && module.parent) {
+if (typeof module !== 'undefined' && module.parent) {
   module.exports = startAdapter;
 } else {
   // or start the instance directly
